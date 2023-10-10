83185237aef94427f16ac3a0e01f4df10ec1d466
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-10 15:58:22 +0900

    ADD: express의 객체화 코드 사용
    .

--- chapter06/app.js
+++ chapter06/app.js
@@ -6,13 +6,18 @@ const path = require('path');
 // .env 파일 가져옴
 dotenv.config();
 const app = express();
+
 // process.env.PORT : 환경변수의 PORT 가져옴
 app.set('port', process.env.PORT || 3000);
 
-// dev 모드의 콘솔 로그 출력
+// morgan : dev 모드의 콘솔 로그 출력
 app.use(morgan('dev'));
-// public 폴더 내부에 있는 사진을 가지고 옴
+ // static : Controller 접근 이전 public 폴더 내부에 있는 사진을 가지고 옴
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+// 요청의 본문을 해석해서 객체화
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello, Express');
