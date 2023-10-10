94596ba20ebe411b252bb9f85fee8ef86edb6549
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-10 15:00:56 +0900

    ADD: express 미들웨어 사용
    .

--- chapter06/app.js
+++ chapter06/app.js
@@ -3,8 +3,23 @@ const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+    console.log('1');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('2');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello, Express');
+});
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send(error.message);
 });
 
 app.listen(app.get('port'), () => {
