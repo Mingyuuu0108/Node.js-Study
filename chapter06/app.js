58a06bce64c71001ad3e2fca4c9f92f3a40c2430
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-10 15:36:49 +0900

    ADD: 이미지 추가하여 morgan 사용

--- chapter06/app.js
+++ chapter06/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const path = require('path');
 
+// .env 파일 가져옴
 dotenv.config();
-
 const app = express();
+// process.env.PORT : 환경변수의 PORT 가져옴
 app.set('port', process.env.PORT || 3000);
 
+// dev 모드의 콘솔 로그 출력
 app.use(morgan('dev'));
+// public 폴더 내부에 있는 사진을 가지고 옴
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('Hello, Express');
