426a426ee6a466a2cc7bed4a239d138ce8cc0441
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-10 15:28:33 +0900

    ADD: morgan 라이브러리 추가 및 사용
    .

--- chapter06/app.js
+++ chapter06/app.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 dotenv.config();
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.use((req, res, next) => {
-    console.log('1');
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('2');
-    next();
-});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Hello, Express');
