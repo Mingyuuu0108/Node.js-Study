3a21af28434064e2e45a21673a63834d3a9a5913
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-11 15:24:01 +0900

    ADD: Router 에서 json 리스폰 구현
    .

--- chapter06/routes/user.js
+++ chapter06/routes/user.js
@@ -9,9 +9,16 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     // query 스트링
-    const limit = req.query.limit ?? 1;
-    res.send(`${req.params.id} : ${limit}`);
+    const name = req.query.name ?? "이민규";
+
+    console.log(req.app.get('port')); // express 의 객체인 app 에 접근 가능한 req.app
+    // res.send(`${req.params.id} : ${limit}`);
+
+    res.json({ // res.json 으로 객체를 json 의 형태로 요청 가능
+        message: `my name is ${name}.`
+    });
 });
+
 // 가변 라우터 아래에 적으면 동작하지 않음, 만약 위에 적었다면 동작함
 router.get('/like', (req, res) => {
     res.send("이 코드는 실행되지 않음.")
