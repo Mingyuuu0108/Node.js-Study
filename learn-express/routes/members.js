d83910387546edb4dda6567337e299361b3c9143
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-11 16:07:53 +0900

    ADD: 1차 과제 query, params 사용
    .

--- learn-express/routes/members.js
+++ learn-express/routes/members.js
@@ -3,12 +3,32 @@ const express = require('express');
 const router = express.Router();
 
 const members = [
-    { memberId: 1, name: 'mingyu' },
-    { memberId: 2, name: 'gyumin' }
+    { memberId: 1, name: '민규', age: 20 },
+    { memberId: 2, name: '규민', age: 17 }
 ]
 
 router.get('/', (req, res) => {
-    res.json(members);
+
+    const name = req.query.name;
+    if (name) {
+        const result = members.filter((members) => {
+            return members.name.includes(name);
+        });
+        res.json(result);
+    } else {
+        res.json(members);
+    }
 });
+
+router.get('/:memberId', (req, res) => {
+    
+    const memberId = req.params.memberId;
+    const result = members.find((member) => member.memberId == memberId);
+    if (result) {
+        res.json(result);
+    } else {
+        res.json({});
+    }
+})
 
 module.exports = router;
