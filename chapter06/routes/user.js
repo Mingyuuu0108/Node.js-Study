b72d50783f6dd6d307ff99805efb75d6e3cd5f42
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-11 14:57:02 +0900

    ADD: express 의 가변 Router 사용

--- chapter06/routes/user.js
+++ chapter06/routes/user.js
@@ -7,4 +7,14 @@ router.get('/', (req, res) => {
     res.send('Hello user :)');
 });
 
+router.get('/:id', (req, res) => {
+    // query 스트링
+    const limit = req.query.limit ?? 1;
+    res.send(`${req.params.id} : ${limit}`);
+});
+// 가변 라우터 아래에 적으면 동작하지 않음, 만약 위에 적었다면 동작함
+router.get('/like', (req, res) => {
+    res.send("이 코드는 실행되지 않음.")
+});
+
 module.exports = router;
