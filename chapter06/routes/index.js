251258d2a68c2c79b0632ab75c40ccf969e235ea
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-17 15:05:10 +0900

    ADD:: index.pug 파일 랜더링

--- chapter06/routes/index.js
+++ chapter06/routes/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    // index.pug 파일을 랜더링
+    res.render('index', {
+        // 데이터 전달
+        title: "Pug Test",
+        items: ['apple', 'melon', 'mango']
+    });
 })
 
 module.exports = router;
