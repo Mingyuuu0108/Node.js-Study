7a9eed1edc551ca1072f8a913ee4c9b53a7b6db4
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-11-14 15:18:59 +0900

    ADD: sns 서비스 카카오톡 로그인 구현
    .

--- nodebird/app.js
+++ nodebird/app.js
@@ -5,10 +5,11 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
-const passport = require('passport')
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -20,9 +21,17 @@ nunjucks.configure('views', {
   express: app,
   watch: true,
 });
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -38,12 +47,11 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// 라우터
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
-// 404
 app.use((req, res, next) => {
-  const error =  new Error(`라우터가 없습니다.`);
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
   next(error);
 });
